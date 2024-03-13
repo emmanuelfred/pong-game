80e1b471fd3e7c65835e162736ed01b0c2c9bf4c
Fredrick Emmanuel <131180996+emmanuelfred@users.noreply.github.com>
2024-03-13 14:41:08 +0100

    inital

---
+++ main.py
@@ -0,0 +1,68 @@
+from kivy.app import App
+from kivy.clock import Clock
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty , ReferenceListProperty, ObjectProperty
+from kivy.vector import  Vector
+from  random import randint
+
+
+
+
+class pongPaddle(Widget):
+    score = NumericProperty(0)
+    def bounce_ball(self,ball):
+        if self.collide_widget(ball):
+            ball.veiocity_x *= -1.1
+class pongBall(Widget):
+    veiocity_x = NumericProperty(0)
+    veiocity_y = NumericProperty(0)
+    veiocity = ReferenceListProperty(veiocity_x , veiocity_y)
+
+    def move(self):
+        self.pos = Vector(*self.veiocity)+ self.pos
+
+class pongGame(Widget):
+    ball = ObjectProperty(None)
+    player1 = ObjectProperty(None)
+    player2 = ObjectProperty(None)
+    def server_ball(self):
+        self.ball.veiocity = Vector(4,0).rotate(randint(0,360))
+    def update(self, dt):
+        self.ball.move()
+
+        if (self.ball.y < 0) or (self.ball.y > self.height - 50):
+            self.ball.veiocity_y *= -1
+
+        if (self.ball.x < 0) :
+            self.ball.veiocity_x *= -1
+            self.player1.score += 1
+
+        if(self.ball.x > self.width - 50):
+            self.ball.veiocity_x *= -1
+            self.player2.score += 1
+
+        self.player1.bounce_ball(self.ball)
+        self.player2.bounce_ball(self.ball)
+
+    def on_touch_move(self, touch):
+        if touch.x < self.width / 1/4:
+            self.player1.center_y = touch.y
+
+        if touch.x > self.width * 3/4:
+            self.player2.center_y = touch.y
+
+
+
+
+
+class pongApp(App):
+    def build(self):
+        game = pongGame()
+        game.server_ball()
+        Clock.schedule_interval(game.update, 1.0/60.0)
+        return game
+
+
+
+
+pongApp().run()
